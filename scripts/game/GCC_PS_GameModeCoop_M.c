88140087cfca322ae3ac6bbafe34644be17ff7c6
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2024-12-13 00:03:11 +0100

    Remove obsolete RL overrides

--- scripts/game/GCC_PS_GameModeCoop_M.c
+++ scripts/game/GCC_PS_GameModeCoop_M.c
@@ -1,30 +1,6 @@
 // Delete after next stable RL update
 modded class PS_GameModeCoop : SCR_BaseGameMode
 {
-	
-	[Attribute("1", UIWidgets.CheckBox, "Every member is moved to their factions HQ channel for a common briefing, instead of just leaders. GC Core feature.", category: "Reforger Lobby")]
-	bool m_factionBriefings;
-	
-	override void OnGameStateChanged()
-	{
-		super.OnGameStateChanged();
-		
-		if (m_factionBriefings && GetState() == SCR_EGameModeState.BRIEFING)
-		{
-			array<int> playerIds = new array<int>();
-			GetGame().GetPlayerManager().GetPlayers(playerIds);
-			
-			PS_VoNRoomsManager VoNRoomsManager = PS_VoNRoomsManager.GetInstance();
-			PS_PlayableManager playableManager = PS_PlayableManager.GetInstance();
-			
-			foreach (int playerId: playerIds)
-			{
-				RplId playableId = playableManager.GetPlayableByPlayer(playerId);
-				if (playableId != RplId.Invalid()) VoNRoomsManager.MoveToRoom(playerId, playableManager.GetPlayerFactionKey(playerId), "#PS-VoNRoom_Command");
-			}
-		}
-	}
-	
 	override void OnGameStart()
 	{
 		super.OnGameStart();
