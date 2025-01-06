b9b382c1299aa66716730307f19000f8cb839dea
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2025-01-05 23:55:05 -0500

    Disable group von room moving

---
+++ scripts/game/Modded/GCC_PS_CharacterSelector.c
@@ -0,0 +1,56 @@
+modded class PS_CharacterSelector
+{
+	override void OnClicked(SCR_ButtonBaseComponent button)
+	{
+		if (m_bStateClickSkip)
+		{
+			m_bStateClickSkip = false;
+			return;
+		}
+		
+		int playerId = m_CoopLobby.GetSelectedPlayer();
+		if (m_iPlayerId > 0 && playerId != m_iPlayerId)
+		{
+			AudioSystem.PlaySound("{C97850E4341F0CF9}Sounds/UI/Samples/Menu/UI_Button_Fail.wav");
+			return;
+		}
+	
+		SCR_EGameModeState gameState = m_GameModeCoop.GetState();
+		if (!PS_PlayersHelper.IsAdminOrServer())
+		{
+			RplId playableId = m_PlayableManager.GetPlayableByPlayer(m_iCurrentPlayerId);
+			if (gameState == SCR_EGameModeState.BRIEFING && playableId != RplId.Invalid())
+				return;
+		}
+		
+		if (playerId != m_iPlayerId)
+		{
+			if (!CanJoinFaction())
+			{
+				SCR_ChatPanelManager chatPanelManager = SCR_ChatPanelManager.GetInstance();
+				ChatCommandInvoker invoker = chatPanelManager.GetCommandInvoker("lmsg");
+				invoker.Invoke(null, "Где баланс?");
+				return;
+			}
+			AudioSystem.PlaySound("{9500A96BBA3B0581}Sounds/UI/Samples/Menu/UI_Gadget_Select.wav");
+			// Disable group von room moving
+			//m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, m_sPlayableCallsign);
+			m_PlayableControllerComponent.ChangeFactionKey(playerId, m_sFactionKey);
+			m_PlayableControllerComponent.SetPlayerState(playerId, PS_EPlayableControllerState.NotReady);	
+			m_PlayableControllerComponent.SetPlayerPlayable(playerId, m_iPlayableId);
+		} else {
+			AudioSystem.PlaySound("{9500A96BBA3B0581}Sounds/UI/Samples/Menu/UI_Gadget_Select.wav");
+			m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, "#PS-VoNRoom_Faction");
+			m_PlayableControllerComponent.ChangeFactionKey(playerId, "");
+			m_PlayableControllerComponent.SetPlayerState(playerId, PS_EPlayableControllerState.NotReady);
+			m_PlayableControllerComponent.SetPlayerPlayable(playerId, RplId.Invalid());
+			if (PS_PlayersHelper.IsAdminOrServer())
+				m_PlayableControllerComponent.UnpinPlayer(playerId);
+		}
+		
+		if (PS_PlayersHelper.IsAdminOrServer() && playerId != m_iCurrentPlayerId && gameState == SCR_EGameModeState.GAME)
+			m_PlayableControllerComponent.ForceSwitch(playerId);
+		if (!PS_PlayersHelper.IsAdminOrServer() && playerId == m_iCurrentPlayerId && gameState == SCR_EGameModeState.BRIEFING)
+			m_PlayableControllerComponent.SwitchToMenuServer(SCR_EGameModeState.BRIEFING);
+	}
+}
