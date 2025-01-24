3356e453ac73adf2a6ed77b1348ed51e5569f870
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2025-01-24 06:21:10 -0500

    Better compatibility

--- scripts/game/Modded/GCC_PS_PlayableControllerComponent.c
+++ scripts/game/Modded/GCC_PS_PlayableControllerComponent.c
@@ -2,37 +2,15 @@ modded class PS_PlayableControllerComponent
 {
 	override void SwitchToObserver(IEntity from)
 	{
-		if (m_eCamera) return;
-		GetGame().GetMenuManager().OpenMenu(ChimeraMenuPreset.SpectatorMenu);
-		PlayerController thisPlayerController = PlayerController.Cast(GetOwner());
-		IEntity entity = thisPlayerController.GetControlledEntity();
-		EntitySpawnParams params = new EntitySpawnParams();
-		if (from) from.GetTransform(params.Transform);
-		MoveToVoNRoom(thisPlayerController.GetPlayerId(), "", "");
-		Resource resource = Resource.Load("{6EAA30EF620F4A2E}Prefabs/Editor/Camera/ManualCameraSpectator.et");
-		m_eCamera = GetGame().SpawnEntityPrefab(resource, GetGame().GetWorld(), params);
-		
-		if (lastCameraTransform[3][1] < 10000 && lastCameraTransform[3][1] > 0)
-		{
-			m_eCamera.SetTransform(lastCameraTransform);
-			lastCameraTransform[3][1] = 10000;
-		} else if (m_vObserverPosition != "0 0 0") { 
-			m_eCamera.SetOrigin(m_vObserverPosition);
-			m_vObserverPosition = "0 0 0";
-		} else {
-			SCR_MapEntity mapEntity = SCR_MapEntity.GetMapInstance();
-			m_eCamera.SetOrigin(mapEntity.Size() / 2.0 + vector.Up * 100);
-		}
-		GetGame().GetCameraManager().SetCamera(CameraBase.Cast(m_eCamera));
+		super.SwitchToObserver(from);
+
 		GetGame().GetCallqueue().CallLater(UpdateSpectator, 100, true);
 	}
 	
 	override void SwitchFromObserver()
 	{
-		if (!m_eCamera) return;
-		GetGame().GetMenuManager().CloseMenuByPreset(ChimeraMenuPreset.SpectatorMenu);
-		SCR_EntityHelper.DeleteEntityAndChildren(m_eCamera);
-		m_eCamera = null;
+		super.SwitchFromObserver();
+
 		GetGame().GetCallqueue().Remove(UpdateSpectator);
 	}
 	
