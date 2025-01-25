986cd674c82703d3ade5d129f4a1396dc3192a78
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-01-25 01:43:59 +0100

    Rename "session end" to "mission end" in end screen, edit JIP hint

--- scripts/game/Player/GCC_SCR_PlayerController_M.c
+++ scripts/game/Player/GCC_SCR_PlayerController_M.c
@@ -8,7 +8,7 @@ modded class SCR_PlayerController : PlayerController
 	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
 	protected void TILW_RpcDo_ShowJIPInfo()
 	{
-		if (GetGame().GetInputManager().IsUsingMouseAndKeyboard()) TILW_ShowHint("You can join-in-progress by holding U, choosing a slot and clicking 'Ready' in the bottom right.", "JIP Available", 15);
-		else TILW_ShowHint("You can join-in-progress by holding RB + Left on D-Pad, choosing a slot and selecting 'Ready' in the bottom right.", "JIP Available", 20);
+		if (GetGame().GetInputManager().IsUsingMouseAndKeyboard()) TILW_ShowHint("You can join-in-progress by holding U, choosing a slot and clicking 'Ready' in the bottom right.\nOnce ingame, double-press U for JIP TP.", "JIP Available", 30);
+		else TILW_ShowHint("You can join-in-progress by holding RB + Left on D-Pad, choosing a slot and selecting 'Ready' in the bottom right.", "JIP Available", 40);
 	}
 }
