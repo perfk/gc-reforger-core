654bcf08053efee36becb49ac08fa27668501d34
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2025-01-30 02:25:34 -0500

    Revert "Merge branch 'master' of https://github.com/Global-Conflicts-ArmA/gc-reforger-core"
    This reverts commit e617e727a9777cf2cb63ee9aca914cd7f3502ac7, reversing
    changes made to 50c6d6fb680f3dd886bac93b896c0361f1271565.

--- scripts/game/Modded/GCC_CSI_Compass_M.c
+++ scripts/game/Modded/GCC_CSI_Compass_M.c
@@ -1,6 +1,60 @@
 modded class CSI_Compass : SCR_InfoDisplay
 {
 	//------------------------------------------------------------------------------------------------
+	override protected void UpdateValues(IEntity owner, float timeSlice)
+	{
+		super.UpdateValues(owner, timeSlice);
+		
+		m_ChimeraCharacter = SCR_ChimeraCharacter.Cast(GetGame().GetPlayerController().GetControlledEntity());
+		
+		if (!m_ChimeraCharacter) 
+			return;
+
+		if (!m_AuthorityComponent || !m_ClientComponent || !m_GroupsManagerComponent || !m_wBearing || !m_wCompass || !m_PlayerManager) 
+		{
+			m_AuthorityComponent = CSI_AuthorityComponent.GetInstance();
+			m_ClientComponent = CSI_ClientComponent.GetInstance();
+			m_GroupsManagerComponent = SCR_GroupsManagerComponent.GetInstance();
+			m_wCompass = ImageWidget.Cast(m_wRoot.FindAnyWidget("Compass"));
+			m_wBearing = TextWidget.Cast(m_wRoot.FindAnyWidget("Bearing"));
+			m_PlayerManager = GetGame().GetPlayerManager();
+			return;
+		};
+		
+		// Get players current group.
+		m_PlayersGroup = m_GroupsManagerComponent.GetPlayerGroup(SCR_PlayerController.GetLocalPlayerId());
+
+		string compassVisible = m_ClientComponent.ReturnLocalCSISettings()[0];
+		string squadRadarVisible = m_ClientComponent.ReturnLocalCSISettings()[1];
+		string hudAutoHidden = m_ClientComponent.ReturnLocalCSISettings()[14];
+
+		if (compassVisible == "false" || (hudAutoHidden == "true" && !hudToggled)) 
+		{
+			if (m_wCompass.GetOpacity() > 0) 
+			{
+				m_wCompass.SetOpacity(0);
+				m_wBearing.SetOpacity(0);
+			};
+		} else {
+			if (m_wCompass.GetOpacity() < 0.625) 
+			{
+				m_wCompass.SetOpacity(0.625);
+				m_wBearing.SetOpacity(0.625);
+			
+				m_sCompassTexture = "{378C1E24252C207D}UI/Textures/HUD/Modded/Compasses/compass_shadow360_nodirs.edds";
+				m_wCompass.LoadImageTexture(0, m_sCompassTexture);
+			};
+		};
+		
+		SetBearingAndCompass(compassVisible, hudAutoHidden);
+
+		if ((squadRadarVisible == "false" || (hudAutoHidden == "true" && !hudToggled)) || !m_PlayersGroup) 
+			ClearSquadRadar(-1);
+		else
+			SquadRadarSearch();
+	}
+
+	//------------------------------------------------------------------------------------------------
 	override protected bool ProcessEntity(IEntity processEntity)
 	{
 		SCR_ChimeraCharacter playerCharacter = SCR_ChimeraCharacter.Cast(processEntity);
