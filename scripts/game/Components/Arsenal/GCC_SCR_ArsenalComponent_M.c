0fffbe6c88e68681d9ae871b2da6e65fc5d9da71
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-03-08 17:46:46 +0100

    Update GCC_SCR_ArsenalComponent_M.c

--- scripts/game/Components/Arsenal/GCC_SCR_ArsenalComponent_M.c
+++ scripts/game/Components/Arsenal/GCC_SCR_ArsenalComponent_M.c
@@ -2,9 +2,6 @@ modded class SCR_ArsenalComponent : ScriptComponent
 {
 	override SCR_EArsenalSaveType GetArsenalSaveType()
 	{
-		BaseGameMode gm = GetGame().GetGameMode();
-		if (gm && PS_GameModeCoop.Cast(gm))
-			return SCR_EArsenalSaveType.SAVING_DISABLED;
-		return m_eArsenalSaveType;
+		return SCR_EArsenalSaveType.SAVING_DISABLED; // m_eArsenalSaveType;
 	}
 }
