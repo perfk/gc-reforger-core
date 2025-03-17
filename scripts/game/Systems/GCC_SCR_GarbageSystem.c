833240cccc11b89a0f019461a4ba336b3fe09a42
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-03-17 18:20:38 +0100

    Prevent cleanup of playable characters

---
+++ scripts/game/Systems/GCC_SCR_GarbageSystem.c
@@ -0,0 +1,20 @@
+//! Script entry for garbage system modding
+modded class SCR_GarbageSystem : GarbageSystem
+{
+	override protected float OnInsertRequested(IEntity entity, float lifetime)
+	{
+		// Prevent insertion of playable characters
+		if (SCR_ChimeraCharacter.Cast(entity))
+		{
+			Managed m = entity.FindComponent(PS_PlayableComponent);
+			if (m)
+			{
+				PS_PlayableComponent plC = PS_PlayableComponent.Cast(m);
+				if (plC.GetPlayable())
+					return -1;
+			}
+		}
+
+		return OnInsertRequested(entity, lifetime);
+	}
+}
