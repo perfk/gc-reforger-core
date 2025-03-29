449a58280a55c3f17b402395a111f4736ad724d6
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-03-29 17:13:35 +0100

    Update GCC_PS_CharacterSelector_M.c

--- scripts/game/Modded/GCC_PS_CharacterSelector_M.c
+++ scripts/game/Modded/GCC_PS_CharacterSelector_M.c
@@ -9,9 +9,23 @@ modded class PS_CharacterSelector
 		}
 		
 		int playerId = m_CoopLobby.GetSelectedPlayer();
+		if (m_iPlayerId == -2)
+		{
+			m_CoopLobby.SetPreviewPlayable(m_iPlayableId, true);
+			SCR_UISoundEntity.SoundEvent("SOUND_FE_BUTTON_FAIL");
+			return;
+		}
 		if (m_iPlayerId > 0 && playerId != m_iPlayerId)
 		{
-			AudioSystem.PlaySound("{C97850E4341F0CF9}Sounds/UI/Samples/Menu/UI_Button_Fail.wav");
+			m_CoopLobby.SetPreviewPlayable(m_iPlayableId, true);
+			SCR_UISoundEntity.SoundEvent("SOUND_FE_BUTTON_FAIL");
+			return;
+		}
+		PS_PlayableContainer playableContainer = m_PlayableManager.GetPlayableById(m_iPlayableId);
+		if (playableContainer.GetDamageState() == EDamageState.DESTROYED)
+		{
+			m_CoopLobby.SetPreviewPlayable(m_iPlayableId, true);
+			SCR_UISoundEntity.SoundEvent("SOUND_FE_BUTTON_FAIL");
 			return;
 		}
 	
@@ -20,7 +34,10 @@ modded class PS_CharacterSelector
 		{
 			RplId playableId = m_PlayableManager.GetPlayableByPlayer(m_iCurrentPlayerId);
 			if (gameState == SCR_EGameModeState.BRIEFING && playableId != RplId.Invalid())
+			{
+				m_CoopLobby.SetPreviewPlayable(m_iPlayableId, true);
 				return;
+			}
 		}
 		
 		if (playerId != m_iPlayerId)
@@ -30,21 +47,24 @@ modded class PS_CharacterSelector
 				SCR_ChatPanelManager chatPanelManager = SCR_ChatPanelManager.GetInstance();
 				ChatCommandInvoker invoker = chatPanelManager.GetCommandInvoker("lmsg");
 				invoker.Invoke(null, "Где баланс?");
+				m_CoopLobby.SetPreviewPlayable(m_iPlayableId, true);
 				return;
 			}
-			AudioSystem.PlaySound("{9500A96BBA3B0581}Sounds/UI/Samples/Menu/UI_Gadget_Select.wav");
-			// Disable group von room moving
-			//m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, m_sPlayableCallsign);
+			
+			SCR_UISoundEntity.SoundEvent("SOUND_HUD_GADGET_SELECT");
+			// GCC m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, m_sPlayableCallsign);
+			
+			// GCC
 			if (gameState == SCR_EGameModeState.BRIEFING)
 				m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, "#PS-VoNRoom_Command");
+			// GCC
 			
 			m_PlayableControllerComponent.ChangeFactionKey(playerId, m_sFactionKey);
 			m_PlayableControllerComponent.SetPlayerState(playerId, PS_EPlayableControllerState.NotReady);	
 			m_PlayableControllerComponent.SetPlayerPlayable(playerId, m_iPlayableId);
 		} else {
-			AudioSystem.PlaySound("{9500A96BBA3B0581}Sounds/UI/Samples/Menu/UI_Gadget_Select.wav");
-			// Disable faction von room moving (unclicking a slot)
-			//m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, "#PS-VoNRoom_Faction");
+			SCR_UISoundEntity.SoundEvent("SOUND_HUD_GADGET_SELECT");
+			// GCC m_PlayableControllerComponent.MoveToVoNRoom(playerId, m_sFactionKey, "#PS-VoNRoom_Faction");
 			m_PlayableControllerComponent.ChangeFactionKey(playerId, "");
 			m_PlayableControllerComponent.SetPlayerState(playerId, PS_EPlayableControllerState.NotReady);
 			m_PlayableControllerComponent.SetPlayerPlayable(playerId, RplId.Invalid());
@@ -61,28 +81,31 @@ modded class PS_CharacterSelector
 	override void OnStateClicked(SCR_ButtonBaseComponent button)
 	{
 		m_bStateClickSkip = true;
-		SCR_EGameModeState gameState = m_GameModeCoop.GetState();
 		switch (m_state)
 		{
 			case PS_ECharacterState.Pin:
-				AudioSystem.PlaySound("{63DB9ACDD10DB801}Sounds/UI/Samples/Editor/UI_E_Layer_Edit_Back.wav");
+				SCR_UISoundEntity.SoundEvent("SOUND_E_LAYER_BACK");
 				m_PlayableControllerComponent.UnpinPlayer(m_iPlayerId);
 				break;
 			case PS_ECharacterState.Dead:
 				break;
 			case PS_ECharacterState.Lock:
-				AudioSystem.PlaySound("{E495F2DA6A44D0BB}Sounds/UI/Samples/Menu/UI_Button_Filter_Off.wav");
+				SCR_UISoundEntity.SoundEvent("SOUND_FE_BUTTON_FILTER_OFF");
 				m_PlayableControllerComponent.SetPlayablePlayer(m_iPlayableId, -1);
 				break;
 			case PS_ECharacterState.Kick:
-				AudioSystem.PlaySound("{5EF75EB4A836831F}Sounds/Explosions/_SharedData/Bodies/Explosion_Body_TNT_Far_01.wav");
-				//m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_sFactionKey, "#PS-VoNRoom_Faction");
+				SCR_UISoundEntity.SoundEvent("SOUND_LOBBY_KICK");
+				// GCC m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_sFactionKey, "#PS-VoNRoom_Faction");
+			
+				// GCC Move to global / command instead
+				SCR_EGameModeState gameState = m_GameModeCoop.GetState();
 				if (gameState == SCR_EGameModeState.BRIEFING)
 					m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_sFactionKey, "#PS-VoNRoom_Command");
-			
+	                        
 				if (gameState == SCR_EGameModeState.SLOTSELECTION)
-					m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_sFactionKey, "#PS-VoNRoom_Global");
-				
+	            		m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_sFactionKey, "#PS-VoNRoom_Global");
+				// GCC
+                                
 				m_PlayableControllerComponent.ChangeFactionKey(m_iPlayerId, "");
 				m_PlayableControllerComponent.SetPlayerState(m_iPlayerId, PS_EPlayableControllerState.NotReady);
 				m_PlayableControllerComponent.SetPlayerPlayable(m_iPlayerId, -1);
@@ -90,7 +113,7 @@ modded class PS_CharacterSelector
 					m_PlayableControllerComponent.UnpinPlayer(m_iPlayerId);
 				break;
 			case PS_ECharacterState.Empty:
-				AudioSystem.PlaySound("{B6008DBCA565E5E1}Sounds/UI/Samples/Menu/UI_Button_Filter_On.wav");
+				SCR_UISoundEntity.SoundEvent("SOUND_FE_BUTTON_FILTER_ON");
 				if (m_iPlayerId > 0)
 					m_PlayableControllerComponent.SetPlayerState(m_iPlayerId, PS_EPlayableControllerState.NotReady);
 				m_PlayableControllerComponent.SetPlayablePlayer(m_iPlayableId, -2);
@@ -98,7 +121,7 @@ modded class PS_CharacterSelector
 			case PS_ECharacterState.Player:
 				break;
 			case PS_ECharacterState.Disconnected:
-				AudioSystem.PlaySound("{5EF75EB4A836831F}Sounds/Explosions/_SharedData/Bodies/Explosion_Body_TNT_Far_01.wav");
+				SCR_UISoundEntity.SoundEvent("SOUND_LOBBY_KICK");
 				m_PlayableControllerComponent.MoveToVoNRoom(m_iPlayerId, m_PlayableManager.GetPlayerFactionKey(m_iPlayerId), "#PS-VoNRoom_Faction");
 				m_PlayableControllerComponent.ChangeFactionKey(m_iPlayerId, "");
 				m_PlayableControllerComponent.SetPlayerPlayable(m_iPlayerId, RplId.Invalid());
