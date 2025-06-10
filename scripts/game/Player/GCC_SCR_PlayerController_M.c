48db4d2f5e698144610ca60334819fa56b6e54e6
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2025-06-11 01:08:21 +0200

    Fix JIP TP key hint

--- scripts/game/Player/GCC_SCR_PlayerController_M.c
+++ scripts/game/Player/GCC_SCR_PlayerController_M.c
@@ -8,7 +8,7 @@ modded class SCR_PlayerController : PlayerController
 	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
 	protected void TILW_RpcDo_ShowJIPInfo()
 	{
-		if (GetGame().GetInputManager().IsUsingMouseAndKeyboard()) TILW_ShowHint("You can join-in-progress by holding U, choosing a slot and clicking 'Ready' in the bottom right.\nOnce ingame, double-press U for JIP TP.", "JIP Available", 30);
+		if (GetGame().GetInputManager().IsUsingMouseAndKeyboard()) TILW_ShowHint("You can join-in-progress by holding U, choosing a slot and clicking 'Ready' in the bottom right.\nOnce ingame, double-press J for JIP TP.", "JIP Available", 30);
 		else TILW_ShowHint("You can join-in-progress by holding RB + Left on D-Pad, choosing a slot and selecting 'Ready' in the bottom right.", "JIP Available", 40);
 	
 		PS_GameModeCoop gm = PS_GameModeCoop.Cast(GetGame().GetGameMode());
