1f1766e60d38b0da4cd74c39b04e10c8e7e073b6
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2025-12-30 19:17:04 -0500

    Hide Radio Names

--- scripts/game/Modded/GCC_SCR_VonDisplay_M.c
+++ scripts/game/Modded/GCC_SCR_VonDisplay_M.c
@@ -7,4 +7,18 @@ modded class SCR_VonDisplay
 		
 		super.OnReceive(playerId, isSenderEditor, receiver, frequency, quality);
 	}
+	
+	override protected bool UpdateTransmission(TransmissionData data, BaseTransceiver radioTransceiver, int frequency, bool IsReceiving)
+	{
+		bool result = super.UpdateTransmission(data, radioTransceiver, frequency, IsReceiving);
+		
+		data.m_Widgets.m_wRole.SetVisible(false);
+		data.m_Widgets.m_wSquadLeaderIcon.SetVisible(false);
+		
+		SCR_EditorManagerEntity editorManager = SCR_EditorManagerEntity.GetInstance();
+		if(!editorManager || !editorManager.IsOpened())
+			data.m_Widgets.m_wName.SetVisible(false);
+		
+		return result;
+	}
 }
